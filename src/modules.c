5fed56fa1dd8dc32a69806ba094ddaed52d5b7f4
unknown <michel.glazenborg@outlook.com>
2017-10-22 12:31:27 +0200

    Added temperature sensor functionality

--- src/modules.c
+++ src/modules.c
@@ -1,11 +1,18 @@
 #include "modules.h"
 
+//Return value of temperature sensor in degrees C
 void read_temperature()
 {
 
 }
 
-void read_lights()
+//Returns value of light sensor between 0 and 100
+float read_lights()
 {
-    
+	ADMUX = _BV(MUX0);
+	ADCSRA = _BV(ADSC);
+	loop_until_bit_is_clear(ADCSRA, ADSC);
+
+	float lightLevel = (float)ADCW / 1024 * 100;
+	return lightLevel;
 }
