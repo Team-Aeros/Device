a993d158090d66080286c61f395f474fa64c10e3
MichelGlazenborg <Michel.Glazenborg@outlook.com>
2017-10-22 15:06:56 +0200

    Updated return statement

--- src/modules.c
+++ src/modules.c
@@ -1,18 +1,17 @@
 #include "modules.h"
 
-//Return value of temperature sensor in degrees C
+// Return value of temperature sensor in degrees C
 void read_temperature()
 {
 
 }
 
-//Returns value of light sensor between 0 and 100
+// Returns value of light sensor between 0 and 100
 float read_lights()
 {
 	ADMUX = _BV(MUX0);
 	ADCSRA = _BV(ADSC);
 	loop_until_bit_is_clear(ADCSRA, ADSC);
 
-	float lightLevel = (float)ADCW / 1024 * 100;
-	return lightLevel;
+	return (float)ADCW / 1024 * 100;
 }
