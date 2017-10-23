f255c29425d39dd6ce844caca699ae9be9ea19dd
Robert Monden <dev@robertmonden.com>
2017-10-23 13:36:27 +0200

    Code update

--- src/main.c
+++ src/main.c
@@ -4,15 +4,21 @@
 #include "status.h"
 #include "scheduler.h"
 
+// 0 = light sensor, 1 = temperature sensor
+const uint8_t MODE = 1;
+
 int main()
 {
+    DDRD = 0xFF;
+    PORTD = 0xFF;
+
     init_connector();
 
     sch_init_t1();
     sch_start();
 
-    sch_add_task(run_temperature_scan, 0, 40);
-    sch_add_task(report_average_temperature, 0, 60);
+    sch_add_task(MODE == 0 ? run_temperature_scan : run_temperature_scan, 0, 40);
+    sch_add_task(MODE == 0 ? report_average_temperature : report_average_temperature, 0, 60);
 
     while (1)
     {
--- src/modules.h
+++ src/modules.h
@@ -1,10 +1,15 @@
 #ifndef dev_modules_h
 #define dev_modules_h
 
+#define MAX_TEMPERATURES 5
+
 float read_temperature();
 float read_lights();
 
 void run_temperature_scan();
 void report_average_temperature();
+void add_temperature_to_average(float temperature);
+
+float average_temperatures[5];
 
 #endif
