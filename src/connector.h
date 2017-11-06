fe28d1fa72d71f93ca2099adc91af256fd527cfd
Robert Monden <dev@robertmonden.com>
2017-11-06 11:15:52 +0100

    Repository update

--- src/connector.h
+++ src/connector.h
@@ -8,6 +8,7 @@
 #define ROLL_UP     0b00100000
 #define ROLL_DOWN   0b00110000
 #define REPORT      0b01000000
+#define CONFIRM     0b01100000
 #define END_TRANSM  0b01110000
 
 #define SETTING_LENGTH          0b00000001
