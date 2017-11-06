aa73ac1a6ac7e5fb090b53f47570c5953b9f620a
Robert Monden <dev@robertmonden.com>
2017-11-06 12:47:10 +0100

    Tabs to spaces

--- src/modules/control.c
+++ src/modules/control.c
@@ -7,16 +7,16 @@
 // Roll up or down
 void roll_shutter(float distance, int direction)
 {
-	float roll_time = distance / 0.05;
-	
-	PORTD &= ~_BV(direction == DOWN ? PD7 : PD6);
-	PORTD |= _BV(direction == DOWN ? PD6 : PD7);
-	
-	for (int i = 0; i < roll_time; i ++)
-	{
-		PORTD |= _BV(PD5);
-		_delay_ms(500);
-		PORTD &= ~_BV(PD5);
-		_delay_ms(500);
-	}
+    float roll_time = distance / 0.05;
+    
+    PORTD &= ~_BV(direction == DOWN ? PD7 : PD6);
+    PORTD |= _BV(direction == DOWN ? PD6 : PD7);
+    
+    for (int i = 0; i < roll_time; i ++)
+    {
+        PORTD |= _BV(PD5);
+        _delay_ms(500);
+        PORTD &= ~_BV(PD5);
+        _delay_ms(500);
+    }
 }
--- src/modules/sensor.c
+++ src/modules/sensor.c
@@ -11,78 +11,78 @@ volatile int status = 1;
 
 float read_sensor()
 {
-	return SENSOR_MODE == 0 ? read_light() : read_temp();
+    return SENSOR_MODE == 0 ? read_light() : read_temp();
 }
 
 void run_sensor_scan()
 {
-	float reading = read_sensor();
-	add_to_average(reading);
-	
-	if (get_average() > roll_down_value) // Change to user chosen variable
-	{
-		if (status == 1)
-		{
-			roll_shutter(length, DOWN);
-			status = 0;
-		}
-	}
-	else
-	{
-		if (status == 0)
-		{
-			roll_shutter(length, UP);
-			status = 1;
-		}
-	}
+    float reading = read_sensor();
+    add_to_average(reading);
+    
+    if (get_average() > roll_down_value) // Change to user chosen variable
+    {
+        if (status == 1)
+        {
+            roll_shutter(length, DOWN);
+            status = 0;
+        }
+    }
+    else
+    {
+        if (status == 0)
+        {
+            roll_shutter(length, UP);
+            status = 1;
+        }
+    }
 
-	transmit(0b11111111);
-	transmit(0b01010000 + status);
-	transmit(0b01110000);
+    transmit(0b11111111);
+    transmit(0b01010000 + status);
+    transmit(0b01110000);
 }
 
 void report_average()
 {
-	float average = get_average() * 10;
-	int result;
+    float average = get_average() * 10;
+    int result;
 
-	transmit(0b11111111);
-	transmit(0b01000000);
+    transmit(0b11111111);
+    transmit(0b01000000);
 
-	while (average >= 0)
-	{
-		if ((result = average - 255) >= 0)
-		{
-			transmit(result);
-			average -= 255;
-		}
-		else
-		{
-			transmit(average);
-		}
-	}
+    while (average >= 0)
+    {
+        if ((result = average - 255) >= 0)
+        {
+            transmit(result);
+            average -= 255;
+        }
+        else
+        {
+            transmit(average);
+        }
+    }
 
-	transmit(0b01110000);
+    transmit(0b01110000);
 }
 
 void add_to_average(float value)
 {
-	for (uint8_t i = 1; i < MAX_VALUES; i++)
-	{
-		average[i - 1] = average[i];
-	}
+    for (uint8_t i = 1; i < MAX_VALUES; i++)
+    {
+        average[i - 1] = average[i];
+    }
 
-	average[MAX_VALUES - 1] = value;
+    average[MAX_VALUES - 1] = value;
 }
 
 float get_average()
 {
-	float sum = 0;
+    float sum = 0;
 
-	for (uint8_t i = 0; i < MAX_VALUES; i++)
-	{
-		sum += average[i];
-	}
+    for (uint8_t i = 0; i < MAX_VALUES; i++)
+    {
+        sum += average[i];
+    }
 
-	return (float) sum / MAX_VALUES;
+    return (float) sum / MAX_VALUES;
 }
