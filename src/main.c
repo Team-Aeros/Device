fa0239e91bb0a1905774e9dc293f5101677da192
Robert Monden <dev@robertmonden.com>
2017-11-09 19:31:53 +0100

    Temperature sensor is still broken

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@ int main()
     init_analog();
 
     scheduler_init_t1();
-    scheduler_start();
 
     int time = SENSOR_MODE == 0 ? 300 : 400;
 
@@ -23,12 +22,36 @@ int main()
     //scheduler_add_task(report_average, 600, 600);
     //scheduler_add_task(check_for_messages, 0, 100);
 
+    scheduler_start();
+
+    PORTD = 0x00;
+
     while (1)
     {
+        int reading = (int) read_sensor();
+
         transmit(0xFF);
         transmit(0b01000000);
-        transmit(read_sensor() * 10);
+        transmit(reading);
         transmit(0b01110000);
+
+        if (reading > 200)
+        {
+            PORTD |= _BV(PD7);
+        }
+        else if (reading > 100)
+        {
+            PORTD |= _BV(PD5);
+        }
+        else if (reading > 20)
+        {
+            PORTD |= _BV(PD6);
+        }
+        else
+        {
+            PORTD = 0xFF;
+        }
+
         _delay_ms(300);
         //scheduler_dispatch_tasks();
     }
